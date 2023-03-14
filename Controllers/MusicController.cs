72441d0ab84bd6cce9da7a70e65e5f75f733b469
WallochMatt <matthewrwalloch@gmail.com>
2023-03-14 13:56:05 -0500

    feat: Created Controllers folder + Created MusicController + Added get and getAll methods

---
+++ Controllers/MusicController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using MusicLibraryWebAPI.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicLibraryWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public MusicController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllMusic() 
+        {
+            var music = _context; //.ModelName
+            return Ok(music);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMusicById(int id) 
+        {
+            var musicById = _context; //.ModelName
+            return Ok(musicById);
+        }
+
+
+
+
+
+    }
+}
